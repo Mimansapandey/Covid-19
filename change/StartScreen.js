350eeac02f22be41e0999cdb068cc8edf01907b3
Mimansa Pandey <65596195+Mimansapandey@users.noreply.github.com>
2020-07-11 16:59:37 +0530

    Add files via upload

---
+++ change/StartScreen.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+    View,
+    Image
+} from 'react-native';
+
+//Trying to import button
+import CustomButton from './CustomButton';
+
+//Import logo
+import bigger from '../assets/Logo(pink).png';
+
+const StartScreen = ({ navigation }) => {
+    return (
+        <View style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: '#fbf4f4'
+        }}>
+            <Image source={bigger} style={{ width: 600, marginBottom: 70, height: 160, }} />
+            <CustomButton text="Sign Up" onPress={() => navigation.navigate('SignUpScreen')}></CustomButton>
+            <CustomButton text="Login" onPress={() => navigation.navigate('SignInScreen')}></CustomButton>
+        </View>
+    );
+};
+
+export default StartScreen;
+
