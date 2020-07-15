70b01a339d77414de8283d5e03284a63dfd58013
syedzarif2237 <zarifyasin@gmail.com>
2020-07-15 16:13:15 +0530

    Dashboard is in data table format

--- GraphicalUI/Components/Dashboard.js
+++ GraphicalUI/Components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
+import { DataTable } from 'react-native-paper';
 
 
 export default function Dashboard() {
@@ -16,20 +17,41 @@ export default function Dashboard() {
   }, []);
 
   return (
-    <View style={{ flex: 1, padding: 24 }}>
-      <Text>id   state   active   confirmed   recovered   deaths</Text>
-      {isLoading ? <ActivityIndicator/> : (
-        <FlatList
+    
+
+    <DataTable>
+    <DataTable.Header>
+      <DataTable.Title >States</DataTable.Title>
+      <DataTable.Title numeric>Active</DataTable.Title>
+      <DataTable.Title numeric>Confirmed</DataTable.Title>
+      <DataTable.Title numeric>Recovered</DataTable.Title>
+      <DataTable.Title numeric>Deaths</DataTable.Title>
+    </DataTable.Header>
+    <FlatList
           data={data}
           keyExtractor={({ id }, index) => id}
           renderItem={({ item }) => (
             
-            <Text>{item.id}, {item.state}, {item.active}, {item.confirmed}, {item.recovered}, {item.deaths} </Text>
+            <DataTable.Row>
+              <DataTable.Cell>{item.state}</DataTable.Cell>
+              <DataTable.Cell numeric>{item.active}</DataTable.Cell>
+              <DataTable.Cell numeric>{item.confirmed}</DataTable.Cell>
+              <DataTable.Cell numeric>{item.recovered}</DataTable.Cell>
+              <DataTable.Cell numeric>{item.deaths}</DataTable.Cell>
+            </DataTable.Row>
+            //<Text>{item.id}, {item.state}, {item.active}, {item.confirmed}, {item.recovered}, {item.deaths} </Text>
             
           )}
         />
-      )}
-    </View>
+    <DataTable.Pagination
+      page={5}
+      numberOfPages={5}
+      onPageChange={page => {
+        console.log(page);
+      }}
+      label="1 of 1"
+    />
+  </DataTable>
+    
   );
 }
-
