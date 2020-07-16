65f8a2be57c99158fc6ba3e014de5d71e68287ff
Mimansa Pandey <65596195+Mimansapandey@users.noreply.github.com>
2020-07-16 13:08:57 +0530

    Update Call.js

--- GraphicalUI/Components/Call.js
+++ GraphicalUI/Components/Call.js
@@ -6,31 +6,248 @@ import { faCalendarPlus } from '@fortawesome/free-solid-svg-icons';
 
 export default function Call() {
 
-    const [modalOpen, setModalOpen] = useState(false);
-    const [modalTwoOpen, setModalTwoOpen] = useState(false);
-
-    const makeCall = () => {
-
-        let phoneNumber = '';
-
-        if (Platform.OS === 'android') {
-            phoneNumber = 'tel:${0201234567}';
-        } else {
-            phoneNumber = 'telprompt:${0201234567}';
-        }
-
-        Linking.openURL(phoneNumber);
-    };
-
     return (
-        <View style={styles.container}>
-            <StatusBar backgroundColor='#32afa9' barStyle="light-content" />
-            <Image
-                        source={require("../assets/helpline.png")}
-                        resizeMode="contain"
-                        style={styles.CallAvatar}
-                    ></Image>
-        </View>
+
+        <DataTable>
+        <DataTable.Header>
+          <DataTable.Title >Name of States</DataTable.Title>
+          <DataTable.Title numeric>Helpline Numbers</DataTable.Title>
+        </DataTable.Header>
+    
+
+        <DataTable.Row>
+    
+          <DataTable.Cell>Andhra Pradesh</DataTable.Cell>
+          <DataTable.Cell numeric>0866-2410978</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Arunachal Pradesh</DataTable.Cell>
+          <DataTable.Cell numeric>9436055743</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Assam</DataTable.Cell>
+          <DataTable.Cell numeric>6913347770</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+         
+          <DataTable.Cell>Bihar</DataTable.Cell>
+          <DataTable.Cell numeric>104</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Chhattisgarh</DataTable.Cell>
+          <DataTable.Cell numeric>104</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+         
+          <DataTable.Cell>Goa</DataTable.Cell>
+          <DataTable.Cell numeric>104</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Gujarat</DataTable.Cell>
+          <DataTable.Cell numeric>104</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Haryana</DataTable.Cell>
+          <DataTable.Cell numeric>8558893911</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+         
+          <DataTable.Cell>Himachal Pradesh</DataTable.Cell>
+          <DataTable.Cell numeric>104</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Jharkhand</DataTable.Cell>
+          <DataTable.Cell numeric>104</DataTable.Cell>
+        </DataTable.Row>
+    
+        <DataTable.Row>
+          
+          <DataTable.Cell>Karnataka</DataTable.Cell>
+          <DataTable.Cell numeric>104</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Kerala</DataTable.Cell>
+          <DataTable.Cell numeric>0471-2552056</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Madhya Pradesh</DataTable.Cell>
+          <DataTable.Cell numeric>104</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Maharashtra</DataTable.Cell>
+          <DataTable.Cell numeric>020-26127394</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Manipur</DataTable.Cell>
+          <DataTable.Cell numeric>3852411668</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+         
+          <DataTable.Cell>Meghalaya</DataTable.Cell>
+          <DataTable.Cell numeric>108</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Mizoram</DataTable.Cell>
+          <DataTable.Cell numeric>102</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Nagaland</DataTable.Cell>
+          <DataTable.Cell numeric>7005539653</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Odisha</DataTable.Cell>
+          <DataTable.Cell numeric>9439994859</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Punjab</DataTable.Cell>
+          <DataTable.Cell numeric>104</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+         
+          <DataTable.Cell>Rajasthan</DataTable.Cell>
+          <DataTable.Cell numeric>0141-2225624</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Sikkim</DataTable.Cell>
+          <DataTable.Cell numeric>104</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Tamil Nadu</DataTable.Cell>
+          <DataTable.Cell numeric>044-29510500</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Telangana</DataTable.Cell>
+          <DataTable.Cell numeric>104</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Tripura</DataTable.Cell>
+          <DataTable.Cell numeric>0381-2315879</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Uttarakhand</DataTable.Cell>
+          <DataTable.Cell numeric>104</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Uttar Pradesh</DataTable.Cell>
+          <DataTable.Cell numeric>18001805145</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+         
+          <DataTable.Cell>West Bengal</DataTable.Cell>
+          <DataTable.Cell numeric>1800313444222, 03323412600</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Andaman and Nicobar islands</DataTable.Cell>
+          <DataTable.Cell numeric>03192-232102</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Chandigarh</DataTable.Cell>
+          <DataTable.Cell numeric>9779558282</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Dadra and Nagar Haveli and Daman & Diu</DataTable.Cell>
+          <DataTable.Cell numeric>104</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Delhi</DataTable.Cell>
+          <DataTable.Cell numeric>011-22307145</DataTable.Cell>
+        </DataTable.Row>
+
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Jammu & Kashmir</DataTable.Cell>
+          <DataTable.Cell numeric>01912520982, 0194-2440283</DataTable.Cell>
+        </DataTable.Row>
+
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Ladakh</DataTable.Cell>
+          <DataTable.Cell numeric>01982256462</DataTable.Cell>
+        </DataTable.Row>
+
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Lakshadweep</DataTable.Cell>
+          <DataTable.Cell numeric>104</DataTable.Cell>
+        </DataTable.Row>
+
+
+        <DataTable.Row>
+          
+          <DataTable.Cell>Puducherry</DataTable.Cell>
+          <DataTable.Cell numeric>104</DataTable.Cell>
+        </DataTable.Row>
+
+    
+    
+        <DataTable.Pagination
+          page={1}
+          numberOfPages={3}
+          onPageChange={page => {
+            console.log(page);
+          }}
+          label="1-2 of 6"
+        />
+      </DataTable>
+    
+        
     );
 }
 
@@ -309,3 +526,4 @@ const styles = StyleSheet.create({
 
 
 });
+
