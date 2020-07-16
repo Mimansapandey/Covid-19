f5e4658db25e9c59e2b820713225edae1380fb14
Mimansa Pandey <65596195+Mimansapandey@users.noreply.github.com>
2020-07-16 13:06:45 +0530

    Update Faq.js

--- GraphicalUI/Components/Faq.js
+++ GraphicalUI/Components/Faq.js
@@ -21,15 +21,21 @@ export default function Faq() {
   // hooks for buttons 
   
   const [answer, setAnswer] = useState('');
-  const handleAnswer = () => setAnswer('Usually eldery people and individuals with a compromised immune system.');
+  const handleAnswer = () => setAnswer('Coronavirus disease (COVID-19) is caused by Novel Coronavirus that leads to cough, fever or difficulty in breathing.');
   const [secondAnswer, setSecondAnswer] = useState('');
-  const handleSecondAnswer = () => setSecondAnswer('The recovery can last from one to two weeks and varies from individual to individual.');
+  const handleSecondAnswer = () => setSecondAnswer('When coughing and sneezing, cover mouth and nose with a tissue, wash hands with soap and water frequently, avoid social contact and if you have any COVID-19 symptoms, contact your nearest healthcare facilty.');
   const [thirdAnswer, setThirdAnswer] = useState('');
-  const handleThirdAnswer = () => setThirdAnswer('The treatment will depend on the condition of the individual. At present time, there is no specific medication for treating COVID-19. Usually, the treatment involves receiving supplemental oxygen and oral antibiotics.');
+  const handleThirdAnswer = () => setThirdAnswer('The virus that causes COVID-19 is thought to spread mainly from person to person through respiratory droplets produced when an infected person coughs, sneezes or talks.Spread is more likely when people are in close contact with one another(within about 6 feet).');
   const [fourthAnswer, setFourthAnswer] = useState('');
-  const handleFourthAnswer = () => setFourthAnswer('Maharashtra, Tamil Nadu and Delhi has the highest number of cases.');
+  const handleFourthAnswer = () => setFourthAnswer('Community spread means people have infected with the virus in an area, including some who are not sure how or where they became infected. Each health department determines community spread differently based on local conditions. For more information on community spread in your area, please visit your health departments website.');
   const [fifthAnswer, setFifthAnswer] = useState('');
-  const handleFifthAnswer = () => setFifthAnswer('Mother-to-child transmission is unlikely, however after your loved one gives a birth, the newborn is susceptible to person-to-person spread.');
+  const handleFifthAnswer = () => setFifthAnswer('Yes. It is possible to test positive for the flu (As well as other respiratory infections) and COVID-19 at the same time"');
+  const [sixthAnswer, setSixthAnswer] = useState('');
+  const handleSixthAnswer = () => setSixthAnswer('Based on currently available information and clinical expertise, older adults and people with underlying medical conditions are at higher risk for severe illness from COVID-19.');
+  const [seventhAnswer, setSeventhAnswer] = useState('');
+  const handleSeventhAnswer = () => setSeventhAnswer('Be alert for symptoms. Watch for fever, cough, shortness of breath, or other symptoms of covid-19. Take your temperature and follow CDC guidance if you have any symptoms.');
+  const [eighthAnswer, setEighthAnswer] = useState('');
+  const handleEighthAnswer = () => setEighthAnswer('Cleaning does not kill germs, but by removing them, it lowers their numbers and risk of spreading infection. if a surface may have gotten the virus on it from a person with or suspected to have COVID-19, the surface should be cleaned and disinfected. Disinfecting kills germs on surfaces.')
 
 
 // using functions to create toggle effect 
@@ -58,6 +64,20 @@ function toggle5() {
   setState5(!state5);
 };
 
+const [state6, setState6] = useState(true);
+function toggle6() {
+  setState6(!state6);
+};
+
+const [state7, setState7] = useState(true);
+function toggle7() {
+  setState7(!state7);
+};
+
+const [state8, setState8] = useState(true);
+function toggle8() {
+  setState8(!state8);
+};
   // check if fonts are loaded
   if (!fontsLoaded) {
     return <AppLoading />;
@@ -67,8 +87,8 @@ function toggle5() {
       <ScrollView style={styles.wrapper}>
 				<StatusBar backgroundColor='#32afa9' barStyle="light-content" />
         <View style={styles.container}>
-          <Text style={styles.bodyTxtBold}>1. Who is at a risk of developing severe illness?</Text>
-          <View><Text style={styles.bodyTxt}>{state ? "" : "Usually eldery people and individuals with a compromised immune system."}</Text></View>
+          <Text style={styles.bodyTxtBold}>1. What is novel Coronavirus Disease?</Text>
+          <View><Text style={styles.bodyTxt}>{state ? "" : "Coronavirus disease (COVID-19) is caused by Novel Coronavirus that leads to cough, fever or difficulty in breathing."}</Text></View>
           {/* <Button title="Test" onPress={handleAnswer}/> */}
         </View>
 
@@ -84,8 +104,8 @@ function toggle5() {
 
 
         <View style={styles.container}>
-          <Text style={styles.bodyTxtBold}>2. How long it takes to fully recover?</Text>
-          <View><Text style={styles.bodyTxt}>{state2 ? "" : "The recovery can last from one to two weeks and varies from individual to individual."}</Text></View>
+          <Text style={styles.bodyTxtBold}>2. How to take precaution/ protect yourself?</Text>
+          <View><Text style={styles.bodyTxt}>{state2 ? "" : "When coughing and sneezing, cover mouth and nose with a tissue, wash hands with soap and water frequently, avoid social contact and if you have any COVID-19 symptoms, contact your nearest healthcare facilty."}</Text></View>
           {/* <Button title="Test" onPress={handleAnswer}/> */}
         </View>
 
@@ -101,8 +121,8 @@ function toggle5() {
 
 
         <View style={styles.container}>
-          <Text style={styles.bodyTxtBold}>3. How will my family member be treated? </Text>
-          <View><Text style={styles.bodyTxt}>{state3 ? "" : "The treatment will depend on the condition of the individual. At present time, there is no specific medication for treating COVID-19. Usually, the treatment involves receiving supplemental oxygen and oral antibiotics."}</Text></View>
+          <Text style={styles.bodyTxtBold}>3. How does the virus spread? </Text>
+          <View><Text style={styles.bodyTxt}>{state3 ? "" : "The virus that causes COVID-19 is thought to spread mainly from person to person through respiratory droplets produced when an infected person coughs, sneezes or talks.Spread is more likely when people are in close contact with one another(within about 6 feet)"}</Text></View>
           {/* <Button title="Test" onPress={handleAnswer}/> */}
         </View>
 
@@ -118,8 +138,40 @@ function toggle5() {
 
 
         <View style={styles.container}>
-          <Text style={styles.bodyTxtBold}>4. Which states have the highest number of cases?</Text>
-          <View><Text style={styles.bodyTxt}>{state4 ? "" : "Maharashtra, Tamil Nadu and Delhi has the highest number of cases.."}</Text></View>
+          <Text style={styles.bodyTxtBold}>4. What is community spread?</Text>
+          <View><Text style={styles.bodyTxt}>{state4 ? "" : "Community spread means people have infected with the virus in an area, including some who are not sure how or where they became infected. Each health department determines community spread differently based on local conditions. For more information on community spread in your area, please visit your health departments website."}</Text></View>
+          {/* <Button title="Test" onPress={handleAnswer}/> */}
+        </View>
+
+        <View style={styles.addBtn}>
+          <TouchableHighlight underlayColor='#fff' onPress={toggle4}>
+            <Image source={add} />
+          </TouchableHighlight>
+        </View>
+
+        <View style={styles.lineCont}>
+          <Image source={line} />
+        </View>
+
+        <View style={styles.container}>
+          <Text style={styles.bodyTxtBold}>5. Is it possible to have the flu and COVID-19 at the same time?</Text>
+          <View><Text style={styles.bodyTxt}>{state4 ? "" : "Yes. It is possible to test positive for the flu (As well as other respiratory infections) and COVID-19 at the same time"}</Text></View>
+          {/* <Button title="Test" onPress={handleAnswer}/> */}
+        </View>
+
+        <View style={styles.addBtn}>
+          <TouchableHighlight underlayColor='#fff' onPress={toggle4}>
+            <Image source={add} />
+          </TouchableHighlight>
+        </View>
+
+        <View style={styles.lineCont}>
+          <Image source={line} />
+        </View>
+
+        <View style={styles.container}>
+          <Text style={styles.bodyTxtBold}>6. Who is at higher risk for serious illness from COVID-19?</Text>
+          <View><Text style={styles.bodyTxt}>{state4 ? "" : "Based on currently available information and clinical expertise, older adults and people with underlying medical conditions are at higher risk for severe illness from COVID-19."}</Text></View>
           {/* <Button title="Test" onPress={handleAnswer}/> */}
         </View>
 
@@ -135,8 +187,25 @@ function toggle5() {
 
 
         <View style={styles.container}>
-          <Text style={styles.bodyTxtBold}>5. Is mother-to-child transmission possible? </Text>
-          <View><Text style={styles.bodyTxt}>{state5 ? "" : "Mother-to-child transmission is unlikely, however after your loved one gives a birth, the newborn is susceptible to person-to-person spread."}</Text></View>
+          <Text style={styles.bodyTxtBold}>7. What should i do if i have had close contact with someone who has COVID-19? </Text>
+          <View><Text style={styles.bodyTxt}>{state5 ? "" : "Be alert for symptoms. Watch for fever, cough, shortness of breath, or other symptoms of covid-19. Take your temperature and follow CDC guidance if you have any symptoms"}</Text></View>
+          {/* <Button title="Test" onPress={handleAnswer}/> */}
+        </View>
+
+        <View style={styles.addBtnLast}>
+          <TouchableHighlight underlayColor='#fff' onPress={toggle5}>
+            <Image source={add} />
+          </TouchableHighlight>
+        </View>
+
+        <View style={styles.lineCont}>
+          <Image source={line} />
+        </View>
+
+
+        <View style={styles.container}>
+          <Text style={styles.bodyTxtBold}>8. Is cleaning alone effective against the virus that causes COVID-19? </Text>
+          <View><Text style={styles.bodyTxt}>{state5 ? "" : "Cleaning does not kill germs, but by removing them, it lowers their numbers and risk of spreading infection. if a surface may have gotten the virus on it from a person with or suspected to have COVID-19, the surface should be cleaned and disinfected. Disinfecting kills germs on surfaces."}</Text></View>
           {/* <Button title="Test" onPress={handleAnswer}/> */}
         </View>
 
@@ -190,6 +259,5 @@ const styles = StyleSheet.create({
     marginTop: 18,
     marginBottom: 17
   }
-
 
 });
