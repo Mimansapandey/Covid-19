60fa42c95dbc96730e07fb1b54a18c82086a53f8
SyedZarif2237 <62008932+SyedZarif2237@users.noreply.github.com>
2020-07-31 18:43:35 +0530

    Add files via upload

--- GraphicalUI/Components/Dashboard.js
+++ GraphicalUI/Components/Dashboard.js
@@ -1,57 +1,126 @@
-import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
+//This is an example code to Add Search Bar Filter on Listview//
+import React, { Component, useEffect, useState } from 'react';
+//import react in our code.
+import {
+  Text,
+  StyleSheet,
+  View,
+  FlatList,
+  TextInput,
+  ActivityIndicator,
+  Alert,
+} from 'react-native';
+//import all the components we are going to use.
 import { DataTable } from 'react-native-paper';
 
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    //setting default state
+    this.state = { isLoading: true, text: '' };
+    this.arrayholder = [];
+  }
 
-export default function Dashboard() {
+  componentDidMount() {
+    return fetch('https://api.covidindiatracker.com/state_data.json')
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState(
+          {
+            isLoading: false,
+            dataSource: responseJson
+          },
+          function() {
+            this.arrayholder = responseJson;
+          }
+        );
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+  SearchFilterFunction(text) {
+    //passing the inserted text in textinput
+    const newData = this.arrayholder.filter(function(item) {
+      //applying filter for the inserted text in search bar
+      const itemData = item.state ? item.state.toUpperCase() : ''.toUpperCase();
+      const textData = text.toUpperCase();
+      return itemData.indexOf(textData) > -1;
+    });
+    this.setState({
+      //setting the filtered newData on datasource
+      //After setting the data it will automatically re-render the view
+      dataSource: newData,
+      text: text,
+    });
+  }
 
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch('https://api.covidindiatracker.com/state_data.json')
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
-  }, []);
-
-  return (
-    
-
-    <DataTable>
-    <DataTable.Header style={{backgroundColor:'#ff4da6'}}>
-      <DataTable.Title >STATES</DataTable.Title>
-      <DataTable.Title numeric>ACTIVE</DataTable.Title>
-      <DataTable.Title numeric>CONFIRMED</DataTable.Title>
-      <DataTable.Title numeric>RECOVERED</DataTable.Title>
-      <DataTable.Title numeric>DEATHS</DataTable.Title>
-    </DataTable.Header>
-    <FlatList
-          data={data}
-          keyExtractor={({ id }, index) => id}
-          renderItem={({ item }) => (
-            
-            <DataTable.Row>
-              <DataTable.Cell>{item.state}</DataTable.Cell>
-              <DataTable.Cell numeric>{item.active}</DataTable.Cell>
-              <DataTable.Cell numeric>{item.confirmed}</DataTable.Cell>
-              <DataTable.Cell numeric>{item.recovered}</DataTable.Cell>
-              <DataTable.Cell numeric>{item.deaths}</DataTable.Cell>
-            </DataTable.Row>
-            //<Text>{item.id}, {item.state}, {item.active}, {item.confirmed}, {item.recovered}, {item.deaths} </Text>
-            
-          )}
+  render() {
+    if (this.state.isLoading) {
+      //Loading View while data is loading
+      return (
+        <View style={{ flex: 1, paddingTop: 20 }}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+    return (
+      //ListView to show with textinput used as search bar
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title>State</DataTable.Title>
+          <DataTable.Title>Confirmed</DataTable.Title>
+          <DataTable.Title>Active</DataTable.Title>
+          <DataTable.Title>Recovered</DataTable.Title>
+          <DataTable.Title>Deceased</DataTable.Title>
+        </DataTable.Header>
+        <View style={styles.viewStyle}>
+          <TextInput
+            style={styles.textInputStyle}
+            onChangeText={text => this.SearchFilterFunction(text)}
+            value={this.state.text}
+            underlineColorAndroid="transparent"
+            placeholder="Search Here"
+          />
+        </View>
+        <FlatList
+              data={this.state.dataSource}
+              keyExtractor={({ id }, index) => id}
+              renderItem={({ item }) => (
+                
+                <DataTable.Row>
+                  <DataTable.Cell>{item.state}</DataTable.Cell>
+                  <DataTable.Cell>{item.confirmed}</DataTable.Cell>
+                  <DataTable.Cell>{item.active}</DataTable.Cell>
+                  <DataTable.Cell>{item.recovered}</DataTable.Cell>
+                  <DataTable.Cell>{item.deaths}</DataTable.Cell>
+                </DataTable.Row>
+              )}
+            />
+        <DataTable.Pagination
+          page={5}
+          numberOfPages={5}
+          onPageChange={page => {
+            console.log(page);
+          }}
+          label="1 of 1"
         />
-    <DataTable.Pagination
-      page={5}
-      numberOfPages={5}
-      onPageChange={page => {
-        console.log(page);
-      }}
-      label="1 of 1"
-    />
-  </DataTable>
-    
-  );
+      </DataTable>
+    );
+  }
 }
+const styles = StyleSheet.create({
+  viewStyle: {
+    justifyContent: 'center',
+    flex: 1,
+    marginTop: 40,
+    padding: 16,
+  },
+  textInputStyle: {
+    height: 40,
+    borderWidth: 1,
+    paddingLeft: 10,
+    borderColor: '#009688',
+    backgroundColor: '#FFFFFF',
+  },
+});
